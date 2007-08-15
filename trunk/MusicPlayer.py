7770eb2382bb8dea9dc1541954d9c53f4ab3f416
noisymime <noisymime@baaa0088-b137-0410-8772-f9fc1f435b5f>
2007-08-15 01:10:53 +0000

    Initial commit

---
+++ trunk/MusicPlayer.py
@@ -0,0 +1,85 @@
+import pygtk
+import gtk
+import clutter
+import eyeD3
+
+class MusicPlayer():
+
+    def __init__(self, stage):
+        self.stage = stage
+        self.cover_viewer = coverViewer(self.stage) 
+        
+        self.is_playing = False
+        
+    def on_key_press_event (self, stage, event):
+        if event.keyval == clutter.keysyms.p:
+            if self.paused:
+                self.unpause()
+            else:
+                self.pause()
+        if event.keyval == clutter.keysyms.q:
+            clutter.main_quit()
+        
+    def begin(self, MenuMgr):
+        
+        #Create a backdrop for the player. In this case we just use the same background as the menus
+        self.backdrop = clutter.CloneTexture(MenuMgr.get_skinMgr().get_Background())
+        self.backdrop.set_size(self.stage.get_width(), self.stage.get_height())
+        self.backdrop.set_opacity(0)
+        self.backdrop.show()
+        self.stage.add(self.backdrop)
+        #Fade the backdrop in
+        timeline_backdrop = clutter.Timeline(10,40)
+        alpha = clutter.Alpha(timeline_backdrop, clutter.ramp_inc_func)
+        backdrop_behaviour = clutter.BehaviourOpacity(alpha, 0, 255)
+        backdrop_behaviour.apply(self.backdrop)
+        timeline_backdrop.start()
+        
+    def stop(self):
+        pass
+        
+    def pause(self):
+        pass
+        
+    def unpause(self):
+        pass
+        
+        
+class coverViewer(clutter.Group):
+
+    def __init__(self, stage):
+        clutter.Group.__init__(self)
+        self.stage = stage
+        self.covers = []
+        self.num_covers = 0
+        self.cover_size = 50 #A cover will be cover_size * cover_size (X * Y)
+        self.cover_gap = 10
+        
+        self.num_rows = 2
+        self.num_columns = int(self.stage.get_width() / self.cover_size)
+        
+    def add_image(self, imagePath):
+        tempTexture = clutter.Texture()
+        pixbuf = gtk.gdk.pixbuf_new_from_file(imagePath)
+        tempTexture.set_pixbuf(pixbuf)
+        xy_ratio = tempTexture.get_width() / tempTexture.get_height()
+        
+        height = int(self.cover_size * xy_ratio)
+        tempTexture.set_width(self.cover_size)
+        tempTexture.set_height(height)
+        
+        self.add(tempTexture)
+        self.num_covers = self.num_covers +1
+        
+        #Redo positioning on all textures to add new one :(
+        """for i = 0 to self.num_covers:
+            tempTexture = self.get_nth_child(i)
+            x = (self.cover_gap + self.cover_size) * i
+            y = (i % self.num_rows) * self.cover_size 
+            tempTexture.set_position(x, y)"""
+        
+
+        
+        
+        
+    
---
+++ trunk/gloss.py
@@ -0,0 +1,97 @@
+import sys
+import clutter
+#import gobject
+import pygtk
+import gtk
+from Menu import Menu
+from MenuMgr import MenuMgr
+from Slideshow import Slideshow
+from VideoPlayer import VideoPlayer
+from MusicPlayer import MusicPlayer
+from TVPlayer import TVPlayer
+
+
+class MainApp:
+    def __init__ (self):
+        gtk.gdk.threads_init()
+    
+        self.stage = clutter.stage_get_default()
+        self.stage.set_color(clutter.color_parse('Black'))
+        #self.stage.set_size(800, 600)
+        self.stage.set_property("fullscreen", True)
+        self.stage.connect('button-press-event', self.on_button_press_event)
+        self.stage.show_all()
+                
+
+        #color = clutter.Color(0xff, 0xcc, 0xcc, 0xdd)
+        color = clutter.Color(0, 0, 0, 0)
+
+        self.menuMgr = MenuMgr(self.stage)
+        self.stage.connect('key-press-event', self.menuMgr.on_key_press_event)
+        
+        menu1 = Menu(self.menuMgr)
+        menu1.addItem("TV", "ui/dvd.png")
+        menu1.addItem("Slideshow", "ui/gallery.png")        
+        menu1.addItem("Videos", "ui/videos.png")
+        menu1.addItem("Link", "ui/link.png")
+        menu1.addItem("nothing", "ui/music.png")
+        menu1.addItem("nothing", "ui/dvd.png")
+        menu1.addItem("nothing", "ui/dvd.png")
+        menu1.addItem("nothing", "ui/dvd.png")
+        
+        #menu1.setListFont('Tahoma 42')
+        menu1.setMenuPositionByName("center")
+        self.menu1 = menu1
+        self.menu1.selectFirst(True)
+        
+        
+        self.menu1.getItemGroup().show_all()
+        
+        self.menu2 = Menu(self.menuMgr)
+        self.menu2.addItem("Nothing", "ui/dvd.png")
+        self.menu2.addItem("Link", "ui/gallery.png")
+        self.menu2.addItem("blah3", "ui/game.png")
+        self.menu2.addItem("blah4", "ui/music.png")
+        self.menu2.addItem("blah", "ui/dvd.png")
+        self.menu2.addItem("blah2", "ui/dvd.png")
+        self.menu2.addItem("blah3", "ui/dvd.png")
+        self.menu2.addItem("blah4", "ui/dvd.png")
+        #self.menu2.setListFont('Tahoma 42')
+        self.menu2.setMenuPositionByName("center")
+        
+        self.mySlideshow = Slideshow(self.menuMgr, "/mythdata/pics")
+        #self.mySlideshow.loadDir("images/", True)
+       
+        self.vidPlayer = VideoPlayer(self.stage)
+        self.tvPlayer = TVPlayer(self.stage)
+        self.musicPlayer = MusicPlayer(self.stage)
+        
+        menu1.getItem(0).setAction(self.tvPlayer)
+        #menu1.getItem(1).setAction(self.mySlideshow)
+        menu1.getItem(1).setAction(self.mySlideshow.generateMenu())
+        menu1.getItem(2).setAction(self.vidPlayer)
+        menu1.getItem(3).setAction(self.menu2)
+        
+        self.menu2.getItem(1).setAction(self.menu1)
+        
+
+
+    def on_button_press_event (self, stage, event):
+        print "mouse button %d pressed at (%d, %d)" % \
+              (event.button, event.x, event.y)
+        if event.button == 1:
+            pass
+        
+    
+    def run (self):
+        self.stage.show()
+        #self.timeline.start()
+        clutter.main()
+
+def main (args):
+    app = MainApp()
+    app.run()
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))

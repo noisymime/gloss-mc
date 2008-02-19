fb95eb225e3c499c7e190aaa2d5e709aaace7ccf
noisymime <noisymime@baaa0088-b137-0410-8772-f9fc1f435b5f>
2008-02-19 00:11:45 +0000

    - Added db lookup for stage size - Known issue with warnings on startup (possibly caused by Slideshow action)

--- gloss.py
+++ gloss.py
@@ -34,25 +34,39 @@ class MainApp:
     
         self.stage = clutter.stage_get_default()
         self.stage.set_color(clutter.color_parse('Black'))
-        self.stage.set_property("fullscreen", True)
-        self.stage.fullscreen()
         self.stage.connect('button-press-event', self.on_button_press_event)
-        self.stage.show_all()
-        
         #hide the cursor
         self.stage.hide_cursor()
         
-        #Display a loading / splash screen
-        self.splashScreen = SplashScr(self.stage)
-        self.splashScreen.display()
         #clutter.threads_enter()
         gobject.timeout_add(500, self.loadGloss)
         #clutter.threads_leave()
         #clutter.threads_add_timeout(500, self.loadGloss())
     
     def loadGloss(self):
+        #Create a master mySQL connection
+        self.dbMgr = mythDB()
+        if not self.dbMgr.connected:
+            return
         
+        #Do an initial lookup for GUI size
+        width = int(self.dbMgr.get_setting("GuiWidth"))
+        height = int(self.dbMgr.get_setting("GuiHeight"))
+        #Set stage size
+        if width == 0 and height == 0:
+            self.stage.fullscreen()
+            self.stage.set_property("fullscreen", True)
+        else:
+            self.stage.set_size(width, height)
+        
+        #now that the size is set, we can show the stage
+        self.stage.show()
+        #Display a loading / splash screen
+        self.splashScreen = SplashScr(self.stage)
+        self.splashScreen.display()
+        #And create the Gloss Manager
         self.glossMgr = GlossMgr(self.stage)
+
         
         #loop through the args
         for arg in self.args:
@@ -63,29 +77,26 @@ class MainApp:
         #Update splash status msg
         self.splashScreen.set_msg("Creating menus")
         MainMenu = self.glossMgr.create_menu() 
-
         #Update splash status msg
         self.splashScreen.set_msg("Connecting to MythTV server")        
-        #Create a master mySQL connection
-        self.dbMgr = mythDB()
         
-        if not self.dbMgr.connected:
-            return
-        
-        #Update splash status msg
+
+        #Load all modules
         for mods in modules:
             #print "Loading mod..."
             tempMod = mods.Module(self.glossMgr, self.dbMgr)
             title =  tempMod.title
             #print title
+            
             self.splashScreen.set_msg("Loading "+title)
             temp_menu_item = MainMenu.addItem(title)
             temp_menu_item.add_image_from_texture(tempMod.menu_image)
             temp_menu_item.setAction(tempMod.action())
-            
+         
         self.splashScreen.remove()
         self.stage.connect('key-press-event', self.glossMgr.on_key_press_event)
         MainMenu.display()
+
         MainMenu.selectFirst(True)
         
        
@@ -100,7 +111,6 @@ class MainApp:
         
     
     def run (self):
-        self.stage.show()
         #self.timeline.start()
         clutter.main()
 

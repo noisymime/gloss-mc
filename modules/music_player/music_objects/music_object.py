97bc2dafe90bda2f963c66d90a416e8955e15eec
noisymime <noisymime@baaa0088-b137-0410-8772-f9fc1f435b5f>
2008-03-10 11:56:40 +0000

    - Much work getting image downloading working due to GDK bug - Now included image_frame element for use with image_grid and image_row

---
+++ modules/music_player/music_objects/music_object.py
@@ -0,0 +1,13 @@
+import gobject
+
+# An abstract class that simply serves to help emit a common signal
+class MusicObject(gobject.GObject):
+    
+    #Setup signals
+    __gsignals__ =  { 
+        "image-found": (
+            gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, [])
+        }
+    
+    def __init__(self):
+        gobject.GObject.__init__(self)
